27ab1bfb3e487727ccdbeae39fdce6e2c6cb7be4
Sandeep Kumar Mahato <sandeepmahato95@gmail.com>
2025-01-16 23:45:03 +0530

    Delete Queries Examples/Joining across databases.sql

--- Queries Examples/Joining across databases.sql
+++
@@ -1,7 +0,0 @@
-USE sql_store; -- sql_store database is currently being used
-
-SELECT order_id, p.product_id, quantity, o.unit_price
-FROM order_items o
-JOIN sql_inventory.products p -- to join product table from 'sql_inventory' database 
--- (Always prefix the table with database name if it's not from the currently used database)
-ON o.product_id = p.product_id
