d060985caecf15466f95a7b6fb862f695ef3f768
Sandeep Kumar Mahato <sandeepmahato95@gmail.com>
2025-01-19 23:35:50 +0530

    Add files via upload

---
+++ Queries Examples/JOINS/INNER JOIN on 3 tables.sql
@@ -0,0 +1,26 @@
+# Joined 3 tables
+
+USE sql_store;
+
+SELECT 
+      o.order_id, 
+      o.order_date, 
+      c.first_name, 
+      c.last_name, 
+      os.name AS status
+      
+FROM customers c
+
+JOIN orders o
+    ON c.customer_id = o.customer_id
+    
+JOIN order_statuses os
+    ON os.order_status_id = o.status
+    
+ORDER BY order_id;
+
+/*
+Tips:
+1. Write down SELECT coloumns name to be displayed from the respective tables and use Alias for easy to understand the result 
+2. Always join tables with Column having similar values
+*/
