be73a57da67fcafc1c48ebf679eb308a8c6db0a9
Sandeep Kumar Mahato <sandeepmahato95@gmail.com>
2025-01-19 23:32:39 +0530

    Update IN operator.sql

--- Queries Examples/IN operator.sql
+++ Queries Examples/IN operator.sql
@@ -1,3 +1,7 @@
 select *
 from products
 where quantity_in_stock in (49,38,72)
+
+-- Using "IN" is essentially the same as writing out several "OR" conditions for the same column, 
+but it is considered cleaner syntax, 
+where quantity_in_stock = 49 OR quantity_in_stock = 38 OR quantity_in_stock = 72
